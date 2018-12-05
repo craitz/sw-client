11eaab4b6babb585242f7966cf13fc89eb9f4d42
Camilo Raitz da Silva <camilo.rsilva@hsl.org.br>
2018-12-05 16:11:32 -0200

    código serviço StarWars

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarWarsService {
+  public planets: any = [];
+  public species: any = [];
+  public films: any = [];
+  public characters: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  private setPlanets() {
-  }
-
-  private setSpecies() {
-  }
-
-  private setFilms() {
-  }
-
-  private setCharacters() {
+  private GetData(url: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.get(url).subscribe(data => {
+        resolve(data);
+       });
+    });
   }
 }
